3239f8325b4e0bae571e8663a8a10c706a66dfc5
Durand Fabrice <oeufdure@gmail.com>
2022-03-25 20:49:58 -0400

    PACKAGE -> MODULE

--- src/modules/rlm_go/all.mk
+++ src/modules/rlm_go/all.mk
@@ -3,14 +3,14 @@
 EMPTY :=
 SPACE := $(EMPTY) $(EMPTY)
 
-PACKAGE  = rlm_go
-BASE_DIR = ${top_srcdir}/src/modules/$(PACKAGE)
+MODULE  = rlm_go
+BASE_DIR = ${top_srcdir}/src/modules/$(MODULE)
 PACKAGE_GOPATH = $(BASE_DIR)/.gopath
 GOPATH   += $(PACKAGE_GOPATH)
 LOCAL_GOPATH = $(subst $(SPACE),:,$(GOPATH))
-BASE     = $(PACKAGE_GOPATH)/src/$(PACKAGE)
+BASE     = $(PACKAGE_GOPATH)/src/$(MODULE)
 
-TARGET = $(PACKAGE).a
+TARGET = $(MODULE).a
 
 # Very dirty hack, to prevent ar from trying to link stuff together. cgo has already taken care of that
 TGT_LINKER := "echo"
@@ -25,23 +25,23 @@ CGO_LDFLAGS += -L${top_srcdir}/build/lib/local/.libs -lfreeradius-radius -lfreer
 CGO_LDFLAGS += $(LDFLAGS)
 
 create_fake_la_files:
-	@echo "/usr/local/lib" > $(top_builddir)/$(BUILD_DIR)/lib/$(PACKAGE).la
-	@echo "$(top_builddir)/$(BUILD_DIR)/lib/local/.libs"  > $(top_builddir)/$(BUILD_DIR)/lib/local/$(PACKAGE).la
+	@echo "/usr/local/lib" > $(top_builddir)/$(BUILD_DIR)/lib/$(MODULE).la
+	@echo "$(top_builddir)/$(BUILD_DIR)/lib/local/.libs"  > $(top_builddir)/$(BUILD_DIR)/lib/local/$(MODULE).la
 
 go_build_static: $(BASE) go_build_dynamic create_fake_la_files
 	cd $(BASE) && \
 	GOPATH='$(LOCAL_GOPATH)' CGO_CFLAGS='$(CGO_CFLAGS)' CGO_LDFLAGS='$(CGO_LDFLAGS)' \
 	go build -buildmode=c-archive \
-	-o $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(PACKAGE).a ./ && \
-	cp $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(PACKAGE).a $(top_builddir)/$(BUILD_DIR)/lib/.libs/$(PACKAGE).a
+	-o $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(MODULE).a ./ && \
+	cp $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(MODULE).a $(top_builddir)/$(BUILD_DIR)/lib/.libs/$(MODULE).a
 
 go_build_dynamic: $(BASE)
 	@echo "CGO_CFLAGS $(CGO_CFLAGS)"
 	cd $(BASE) && \
 	GOPATH='$(LOCAL_GOPATH)' CGO_CFLAGS='$(CGO_CFLAGS)' CGO_LDFLAGS='$(CGO_LDFLAGS)' \
 	go build -buildmode=c-shared \
-	-o $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(PACKAGE).so ./ && \
-	cp $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(PACKAGE).so $(top_builddir)/$(BUILD_DIR)/lib/.libs/$(PACKAGE).so
+	-o $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(MODULE).so ./ && \
+	cp $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(MODULE).so $(top_builddir)/$(BUILD_DIR)/lib/.libs/$(MODULE).so
 
 $(BASE):
 	@mkdir -p $(dir $@)
