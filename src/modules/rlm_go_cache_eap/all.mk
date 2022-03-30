0fe5ab6b4458660889ac9963e8fb6f7904df3ace
Durand Fabrice <oeufdure@gmail.com>
2022-03-29 21:46:50 -0400

    Renamed in all.mk

--- src/modules/rlm_go_cache_eap/all.mk
+++ src/modules/rlm_go_cache_eap/all.mk
@@ -24,18 +24,18 @@ CGO_LDFLAGS := -Wl,--unresolved-symbols=ignore-all
 CGO_LDFLAGS += -L${top_srcdir}/build/lib/local/.libs -lfreeradius-radius -lfreeradius-server
 CGO_LDFLAGS += $(LDFLAGS)
 
-create_fake_la_files:
+create_fake_la_files_rlm_go_cache_eap:
 	@echo "/usr/local/lib" > $(top_builddir)/$(BUILD_DIR)/lib/$(MODULE).la
 	@echo "$(top_builddir)/$(BUILD_DIR)/lib/local/.libs"  > $(top_builddir)/$(BUILD_DIR)/lib/local/$(MODULE).la
 
-go_build_static: $(BASE) go_build_dynamic create_fake_la_files
+go_build_static_rlm_go_cache_eap: $(BASE) go_build_dynamic_rlm_go_cache_eap create_fake_la_files_rlm_go_cache_eap
 	cd $(BASE) && \
 	GOPATH='$(LOCAL_GOPATH)' CGO_CFLAGS='$(CGO_CFLAGS)' CGO_LDFLAGS='$(CGO_LDFLAGS)' \
 	go build -buildmode=c-archive \
 	-o $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(MODULE).a ./ && \
 	cp $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(MODULE).a $(top_builddir)/$(BUILD_DIR)/lib/.libs/$(MODULE).a
 
-go_build_dynamic: $(BASE)
+go_build_dynamic_rlm_go_cache_eap: $(BASE)
 	@echo "CGO_CFLAGS $(CGO_CFLAGS)"
 	cd $(BASE) && \
 	GOPATH='$(LOCAL_GOPATH)' CGO_CFLAGS='$(CGO_CFLAGS)' CGO_LDFLAGS='$(CGO_LDFLAGS)' \
@@ -47,11 +47,11 @@ $(BASE):
 	@mkdir -p $(dir $@)
 	@ln -sf $(BASE_DIR) $@
 
-build/lib/local/rlm_go_cache_eap.la: go_build_static
+build/lib/local/rlm_go_cache_eap.la: go_build_static_rlm_go_cache_eap
 
-build/lib/local/rlm_go_cache_eap.so: go_build_dynamic
+build/lib/local/rlm_go_cache_eap.so: go_build_dynamic_rlm_go_cache_eap
 
-clean_rlm_go_cache_eap.la: go_clean
+clean_rlm_go_cache_eap.la: go_clean_rlm_go_cache_eap
 
-go_clean:
+go_clean_rlm_go_cache_eap:
 	@rm -rf $(GOPATH)
