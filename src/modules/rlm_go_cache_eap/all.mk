6c1c52f7a9e7ae53f44231a117c4a85da05c8f2a
Durand Fabrice <oeufdure@gmail.com>
2022-03-29 22:11:39 -0400

    try to fix

--- src/modules/rlm_go_cache_eap/all.mk
+++ src/modules/rlm_go_cache_eap/all.mk
@@ -1,4 +1,4 @@
-#.PHONY: go_clean go_build_static go_build_dynamic
+#.PHONY: go_clean_rlm_cache_eap go_build_static_rlm_go_cache_eap go_build_dynamic_rlm_go_cache_eap
 
 EMPTY :=
 SPACE := $(EMPTY) $(EMPTY)
@@ -8,7 +8,7 @@ BASE_DIR_RLM_GO_CACHE_EAP = ${top_srcdir}/src/modules/$(MODULE)
 PACKAGE_GOPATH = $(BASE_DIR_RLM_GO_CACHE_EAP)/.gopath
 GOPATH   += $(PACKAGE_GOPATH)
 LOCAL_GOPATH = $(subst $(SPACE),:,$(GOPATH))
-BASE     = $(PACKAGE_GOPATH)/src/$(MODULE)
+BASE_RLM_GO_CACHE_EAP     = $(PACKAGE_GOPATH)/src/$(MODULE)
 
 TARGET = $(MODULE).a
 
@@ -28,22 +28,22 @@ create_fake_la_files_rlm_go_cache_eap:
 	@echo "/usr/local/lib" > $(top_builddir)/$(BUILD_DIR)/lib/$(MODULE).la
 	@echo "$(top_builddir)/$(BUILD_DIR)/lib/local/.libs"  > $(top_builddir)/$(BUILD_DIR)/lib/local/$(MODULE).la
 
-go_build_static_rlm_go_cache_eap: $(BASE) go_build_dynamic_rlm_go_cache_eap create_fake_la_files_rlm_go_cache_eap
-	cd $(BASE) && \
+go_build_static_rlm_go_cache_eap: $(BASE_RLM_GO_CACHE_EAP) go_build_dynamic_rlm_go_cache_eap create_fake_la_files_rlm_go_cache_eap
+	cd $(BASE_RLM_GO_CACHE_EAP) && \
 	GOPATH='$(LOCAL_GOPATH)' CGO_CFLAGS='$(CGO_CFLAGS)' CGO_LDFLAGS='$(CGO_LDFLAGS)' \
 	go build -buildmode=c-archive \
 	-o $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(MODULE).a ./ && \
 	cp $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(MODULE).a $(top_builddir)/$(BUILD_DIR)/lib/.libs/$(MODULE).a
 
-go_build_dynamic_rlm_go_cache_eap: $(BASE)
+go_build_dynamic_rlm_go_cache_eap: $(BASE_RLM_GO_CACHE_EAP)
 	@echo "CGO_CFLAGS $(CGO_CFLAGS)"
-	cd $(BASE) && \
+	cd $(BASE_RLM_GO_CACHE_EAP) && \
 	GOPATH='$(LOCAL_GOPATH)' CGO_CFLAGS='$(CGO_CFLAGS)' CGO_LDFLAGS='$(CGO_LDFLAGS)' \
 	go build -buildmode=c-shared \
 	-o $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(MODULE).so ./ && \
 	cp $(top_builddir)/$(BUILD_DIR)/lib/local/.libs/$(MODULE).so $(top_builddir)/$(BUILD_DIR)/lib/.libs/$(MODULE).so
 
-$(BASE):
+$(BASE_RLM_GO_CACHE_EAP):
 	@mkdir -p $(dir $@)
 	@ln -sf $(BASE_DIR_RLM_GO_CACHE_EAP) $@
 
